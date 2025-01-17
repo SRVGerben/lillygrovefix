f9f6a11f29aa9f3e169ca97a15bcd8d755c4b617
Gerben Schevelenbosch <gerben.schevelenbosch@student.uhasselt.be>
2025-01-17 19:30:35 +0100

    Initial commit

---
+++ src/main/java/net/gerben/lillygrovefix/LillyGroveFixClient.java
@@ -0,0 +1,10 @@
+package net.gerben.lillygrovefix;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LillyGroveFixClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
